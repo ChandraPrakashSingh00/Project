46c52981372157281fb411517806d21aed081c0f
Chirag Sharma <10406chiragsharma@gmail.com>
2025-12-24 14:37:39 +0530

    fixed env variables

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const AdminRoutes = require("./routes/DashRoutes")
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
   })
